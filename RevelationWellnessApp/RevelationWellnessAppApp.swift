88dd497f8e76ba524dc6b79bf29aff92308817d8
Brian Alonzo <connect@brianalonzo.com>
2024-08-23 10:45:36 -0400

    Initial Commit

---
+++ RevelationWellnessApp/RevelationWellnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevelationWellnessAppApp.swift
+//  RevelationWellnessApp
+//
+//  Created by Brian Alonzo on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct RevelationWellnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
